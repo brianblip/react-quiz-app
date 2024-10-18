68d22fd6a3663eee806e0d96643a9827749ef929
Brian Prado <67305006+brianblip@users.noreply.github.com>
2024-10-18 21:27:53 +0800

    undo default page

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,37 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+          <img src={reactLogo} className="App-logo" alt="logo" />
+          <img src={viteLogo} className="App-logo" alt="logo" />
+          <h1>Hello Vite + React!</h1>
+          <p>
+            <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
+          </p>
+          <p>
+            Edit <code>App.jsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+          
+        </div>
     </>
   )
 }
